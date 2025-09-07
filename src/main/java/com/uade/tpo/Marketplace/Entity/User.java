45ae3bbb5c7b132b12d529a7fb90a067a9d42447
NahuelCuri <curin1050@protonmail.com>
2025-09-07 19:43:20 -0300

    Added JWT and SpringSecurity.

--- src/main/java/com/uade/tpo/Marketplace/Entity/User.java
+++ src/main/java/com/uade/tpo/Marketplace/Entity/User.java
@@ -1,15 +1,26 @@
 package com.uade.tpo.Marketplace.Entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor; // Add this
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor 
 @Entity
-@Table(name = "users") 
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,5 +48,45 @@ public class User {
 
     @OneToMany(mappedBy = "buyer")
     private List<Order> orders;
+
+    @Builder
+    private User(String username, String password, String email, List<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
---
+++ src/main/java/com/uade/tpo/Marketplace/Service/AuthenticationService.java
@@ -0,0 +1,69 @@
+package com.uade.tpo.Marketplace.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.uade.tpo.Marketplace.Config.AuthenticationRequest;
+import com.uade.tpo.Marketplace.Config.AuthenticationResponse;
+import com.uade.tpo.Marketplace.Config.JwtService;
+import com.uade.tpo.Marketplace.Config.RegisterRequest;
+import com.uade.tpo.Marketplace.Entity.User;
+import com.uade.tpo.Marketplace.Repository.RoleRepository;
+import com.uade.tpo.Marketplace.Repository.UserRepository;
+
+import com.uade.tpo.Marketplace.Entity.Role;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+        @Autowired
+        private UserRepository repository;
+        @Autowired
+        private RoleRepository roleRepository;
+        @Autowired
+        private PasswordEncoder passwordEncoder;
+        @Autowired
+        private JwtService jwtService;
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Transactional
+        public AuthenticationResponse register(RegisterRequest request) {
+                Role role = roleRepository.findByName(request.getRole())
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+
+                User user = User.builder()
+                                .username(request.getUsername())
+                                .email(request.getEmail())
+                                .password(passwordEncoder.encode(request.getPassword()))
+                                .roles(List.of(role))
+                                .build();
+
+                repository.save(user);
+                var jwtToken = jwtService.generateToken(user);
+                return AuthenticationResponse.builder()
+                                .accessToken(jwtToken)
+                                .build();
+        }
+
+        public AuthenticationResponse authenticate(AuthenticationRequest request) {
+                authenticationManager.authenticate(
+                                new UsernamePasswordAuthenticationToken(
+                                                request.getEmail(),
+                                                request.getPassword()));
+                var user = repository.findByEmail(request.getEmail())
+                                .orElseThrow();
+                var jwtToken = jwtService.generateToken(user);
+                return AuthenticationResponse.builder()
+                                .accessToken(jwtToken)
+                                .build();
+        }
+}
